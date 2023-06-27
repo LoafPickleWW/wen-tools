de1d850b137551705b3a05984c950e5b3e18dce0
LoafPickleWW <95520576+LoafPickleWW@users.noreply.github.com>
2023-06-27 13:34:47 -0400

    Update ARC19UpdateTool.jsx
    Fixed font

--- src/pages/ARC19UpdateTool.jsx
+++ src/pages/ARC19UpdateTool.jsx
@@ -265,8 +265,8 @@ export function ARC19UpdateTool() {
           here
         </a>
       </p>
-      <p className="text-2xl text-white font-roboto -mt-2 mb-2">
-        This tool is not compatible with NFTs minted from algonfts.art.
+      <p className="text-xl text-red font-roboto -mt-2 mb-2">
+        ⚠️This tool is not compatible with NFTs minted from algonfts.art⚠️
       </p>
       <p>3- Upload CSV file</p>
       {csvData == null ? (
