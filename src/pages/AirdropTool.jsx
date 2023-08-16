891bbc6be087ef0f2a0a6803fc2c9d82f08f339e
LoafPickleWW <95520576+LoafPickleWW@users.noreply.github.com>
2023-08-16 10:20:50 -0400

    Update AirdropTool.jsx
    Changed ASA to Horse

--- src/pages/AirdropTool.jsx
+++ src/pages/AirdropTool.jsx
@@ -149,7 +149,7 @@ export function AirdropTool() {
           rel="noopener noreferrer"
           className="text-slate-500 hover:text-slate-300 transition"
         >
-          ASA from Thurstober Digital Studios
+          Horse from Thurstober Digital Studios
         </a>
         , you can use note field too.
       </p>
