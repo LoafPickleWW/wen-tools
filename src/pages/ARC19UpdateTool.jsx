2424719361757c3d08b0b2a00c68a256ab18ad1b
LoafPickleWW <loafpickle@protonmail.com>
2023-11-30 09:24:09 -0500

    Update ARC19UpdateTool.jsx
    Added a wipe note field to hopefully clear the issue with arc19/69

--- src/pages/ARC19UpdateTool.jsx
+++ src/pages/ARC19UpdateTool.jsx
@@ -66,6 +66,7 @@ export function ARC19UpdateTool() {
       const asset_id = item.asset_id;
       const name = item.name;
       let ipfs_cid = item.image_ipfs_cid;
+      const asset_note = "";
 
       if (ipfs_cid && ipfs_cid.startsWith("ipfs://")) {
         ipfs_cid = ipfs_cid.replace("ipfs://", "");
@@ -103,6 +104,7 @@ export function ARC19UpdateTool() {
       const transaction_data = {
         asset_id,
         ipfs_data,
+        note: asset_note,
       };
       data_for_txns.push(transaction_data);
     });
