a9fe15ddee335fbed11a1a50c3818c5ed99eb24c
LoafPickleWW <loafpickle@protonmail.com>
2023-12-05 13:52:16 -0500

    Update BatchOptin.jsx
    Removed Space Bug

--- src/pages/BatchOptin.jsx
+++ src/pages/BatchOptin.jsx
@@ -236,7 +236,7 @@ export function BatchOptin() {
         id="copy-link"
         className="mb-2 bg-green-500 hover:bg-green-700 text-black text-sm font-semibold rounded py-1 w-fit px-4 mx-auto mt-1 hover:scale-95 duration-700"
         onClick={() => {
-          navigator.clipboard.writeText(window.location.href.split("?")[0] + "?ids=" + assetIds);
+          navigator.clipboard.writeText(window.location.href.split("?")[0] + "?ids=" + assetIds.replace("\n", ","));
           toast.success("Link copied!");
         }}
         >
