7985568011b9d71a1ea248e0a393867069cb9585
LoafPickleWW <loafpickle@protonmail.com>
2023-12-05 11:28:32 -0500

    Update BatchOptin.jsx
    added useEffect import

--- src/pages/BatchOptin.jsx
+++ src/pages/BatchOptin.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //import Papa from "papaparse";
 import ConnectButton from "../components/ConnectButton";
 import algosdk from "algosdk";
