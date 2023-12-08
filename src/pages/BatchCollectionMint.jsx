bd76572b8cee1753d5dfedd2b3502c91ce10abc0
LoafPickleWW <loafpickle@protonmail.com>
2023-12-08 11:59:26 -0500

    Update BatchCollectionMint.jsx
    Added Fee Price

--- src/pages/BatchCollectionMint.jsx
+++ src/pages/BatchCollectionMint.jsx
@@ -303,6 +303,7 @@ export function BatchCollectionMint() {
           )}
         </div>
       )}
+      <p className="text-sm italic text-slate-200">Fee: 0.1A/ASA</p>
       <p className="text-center text-xs text-slate-400 py-2">
         ⚠️If you reload or close this page, you will lose your progress⚠️
         <br />
