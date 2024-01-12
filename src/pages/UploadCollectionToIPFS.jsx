4e937f827566a75b0881692895ecbc603479e2b3
LoafPickleWW <loafpickle@protonmail.com>
2024-01-12 13:14:54 -0500

    Update UploadCollectionToIPFS.jsx
    Fixed?

--- src/pages/UploadCollectionToIPFS.jsx
+++ src/pages/UploadCollectionToIPFS.jsx
@@ -24,7 +24,7 @@ export function UploadCollectionToIPFS() {
     }
     try {
       setLoading(true);
-      const cid = await pinJSONToNFTStorage(token, jsonString);
+      const cid = await client.storeDirectory([selectedFiles]);
       setCollectionCid(cid);
       navigator.clipboard.writeText(cid);
       toast.success("Your cid copied to clipboard!");
