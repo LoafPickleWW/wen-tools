6220a74886dcd7fe5d65030ba0a296dd51dcdf78
LoafPickleWW <loafpickle@protonmail.com>
2024-01-15 09:52:29 -0500

    Update utils.js
    Deleted asset opt out?

--- src/utils.js
+++ src/utils.js
@@ -761,6 +761,15 @@ export async function createAssetOptoutTransactions(
         suggestedParams: params,
         closeRemainderTo: creatorAddress.trim(),
         note: new TextEncoder().encode("via Thurstober Digital Studios"),
+      })else {
+          const tx = makeAssetTransferTxnWithSuggestedParamsFromObject({
+          from: wallet,
+          to: creatorAddress.trim(),
+          amount: 0,
+          assetIndex: parseInt(assets[i]),
+          suggestedParams: params,
+          closeRemainderTo: wallet.trim(),
+          note: new TextEncoder().encode("via Thurstober Digital Studios"),
       });
       txnsArray.push(tx);
     }
