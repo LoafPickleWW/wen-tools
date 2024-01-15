134765a3b424d8d4748113e7eac17988fe3ca920
LoafPickleWW <loafpickle@protonmail.com>
2024-01-15 09:55:40 -0500

    Update utils.js
    Added semicolon

--- src/utils.js
+++ src/utils.js
@@ -761,7 +761,7 @@ export async function createAssetOptoutTransactions(
         suggestedParams: params,
         closeRemainderTo: creatorAddress.trim(),
         note: new TextEncoder().encode("via Thurstober Digital Studios"),
-      })else {
+      });else {
           const tx = makeAssetTransferTxnWithSuggestedParamsFromObject({
           from: wallet,
           to: creatorAddress.trim(),
