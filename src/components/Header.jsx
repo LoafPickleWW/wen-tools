7e21e3785fd49bd6266cf22c9aa2cd6d02a58376
algovado <90692255+algovado@users.noreply.github.com>
2024-03-11 19:45:25 +0300

    add waffi support

---
+++ src/components/Header.jsx
@@ -0,0 +1,57 @@
+import ConnectButton from "./ConnectButton";
+
+export const Header = () => {
+  return (
+    <>
+      <header className="flex flex-row items-center justify-between border-gray-200 px-4 md:px-8 py-3 bg-gray-800">
+        <div className="flex items-s">
+          <div className="mt-2">
+            <a href="/">
+              <img
+                src="./TL_large_white.png"
+                className="h-12 hover:scale-95 duration-700"
+                alt="The Laboratory"
+              />
+            </a>
+          </div>
+          <div className="ml-2">
+            <a href="https://thurstober.com" className="flex items-center">
+              <img
+                className="h-[70px] hover:scale-95 duration-700"
+                src="./cherry.png"
+                alt="logo"
+              />
+            </a>
+          </div>
+        </div>
+        <ConnectButton />
+      </header>
+      <div className="bg-primary-green text-black flex py-1 justify-center items-center">
+        <p className="text-center text-sm">
+          You can read more about Infinity Mode{" "}
+          <a
+            href="https://loafpickle.medium.com/evil-tools-infinity-mode-1bd70ec71c2b"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold hover:text-green-800 transition"
+          >
+            🔗 here!
+          </a>
+        </p>
+      </div>
+      <div className="bg-secondary-yellow text-black flex py-1 justify-center items-center">
+        <p className="text-center text-sm ticker-text">
+          Improve your wallet experience with the: {" "}
+          <a
+            href="https://we.thurstober.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="font-semibold hover:text-green-800 transition"
+          >
+            Wallet Enhancer!
+          </a>
+        </p>
+      </div>
+    </>
+  );
+};
