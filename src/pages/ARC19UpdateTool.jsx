b9abb57fa35fd4b450c2c7a053b55a0bb0449f15
LoafPickleWW <loafpickle@protonmail.com>
2024-03-26 16:29:46 -0400

    Update ARC19UpdateTool.jsx
    Added animation stuff

--- src/pages/ARC19UpdateTool.jsx
+++ src/pages/ARC19UpdateTool.jsx
@@ -77,6 +77,8 @@ export function ARC19UpdateTool() {
         image: ipfs_cid ? "ipfs://" + ipfs_cid : "",
         image_mime_type: item.mime_type,
         description: item.description,
+        animation_url: item.animation_url,
+        animation_mime_type: item.animation_mime_type,
         properties: {
         traits: {},  
         filters: {}
