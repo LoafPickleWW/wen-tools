a52ec74e9128a3af73b3b1dccec076ed69c0cf7b
LoafPickleWW <loafpickle@protonmail.com>
2024-05-21 10:55:55 -0400

    Update tailwind.config.js
    Darker?

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         "primary-gray": "#D0D0D1",
         "secondary-gray": "#393C3A",
         "secondary-black": "#000a02",
-        "banner-grey": "#44403c",
+        "banner-grey": "#262626",
         "text-white": "#f9fafb",        
       },
       fontSize: {
