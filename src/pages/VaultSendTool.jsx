147bfc6eccc49315453a0eb755164bdcf4cb86f7
LoafPickleWW <loafpickle@protonmail.com>
2024-05-21 10:32:36 -0400

    Update VaultSendTool.jsx
    Added Opt In Fee message

--- src/pages/VaultSendTool.jsx
+++ src/pages/VaultSendTool.jsx
@@ -287,7 +287,7 @@ export function VaultSendTool() {
           rel="noopener noreferrer"
           href="https://api-docs.nf.domains/reference/integrators-guide/using-vaults-2.x+"
         >
-          here
+          here. Please note you will need to fund the Opt In for the Vault (0.1A)/Asset.
         </a>{" "}
         for more details.
       </span>
