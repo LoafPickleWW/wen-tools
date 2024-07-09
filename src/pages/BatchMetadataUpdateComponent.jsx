5eb9c972d83f54c7f80ccd56375a3c8c8642353d
LoafPickleWW <loafpickle@protonmail.com>
2024-07-09 12:30:20 -0400

    Update BatchMetadataUpdateComponent.jsx
    Colors

--- src/pages/BatchMetadataUpdateComponent.jsx
+++ src/pages/BatchMetadataUpdateComponent.jsx
@@ -128,9 +128,9 @@ export function BatchCollectionMetadataUpdate() {
       {/* mnemonic */}
       <InfinityModeComponent mnemonic={mnemonic} setMnemonic={setMnemonic} />
       {/* end mnemonic */}
-      <button className="text-center text-lg text-pink-200 mt-2 bg-pink-700 px-4 py-2 rounded">
+      <button className="text-center text-lg text-black mt-2 bg-black px-4 py-2 rounded">
         <a
-          className="hover:text-primary-green transition"
+          className="hover:text-black transition"
           href="https://loafpickle.medium.com/evil-tools-arc69-made-easy-c7913885cfd2"
           target="_blank"
           rel="noopener noreferrer"
@@ -175,7 +175,7 @@ export function BatchCollectionMetadataUpdate() {
         <div className="flex flex-col justify-center items-center w-[16rem]">
           {isTransactionsFinished ? (
             <>
-              <p className="pt-4 text-green-500 animate-pulse text-sm">
+              <p className="pt-4 text-primary-green animate-pulse text-sm">
                 All transactions completed!
                 <br />
               </p>
@@ -193,7 +193,7 @@ export function BatchCollectionMetadataUpdate() {
               {!txSendingInProgress ? (
                 <button
                   id="approve-send"
-                  className="mb-2 bg-green-500 hover:bg-green-700 text-black text-base font-semibold rounded py-2 w-fit px-2 mx-auto mt-1 hover:scale-95 duration-700"
+                  className="mb-2 bg-primary-green hover:bg-green-700 text-black text-base font-semibold rounded py-2 w-fit px-2 mx-auto mt-1 hover:scale-95 duration-700"
                   onClick={handleFileData}
                 >
                   Approve & Send
