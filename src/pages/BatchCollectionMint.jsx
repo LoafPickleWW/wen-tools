6b451905dcea605351d93f11f59622aaa15af0e3
LoafPickle <loafpickle@protonmail.com>
2024-08-16 12:03:01 -0400

    Update BatchCollectionMint.jsx

--- src/pages/BatchCollectionMint.jsx
+++ src/pages/BatchCollectionMint.jsx
@@ -274,7 +274,7 @@ export function BatchCollectionMint() {
           )}
         </div>
       )}
-      <p className="text-sm italic text-slate-200">Fee: 0.1A/ASA</p>
+      <p className="text-sm italic text-slate-200">Fee: Free</p>
       <p className="text-center text-xs text-slate-400 py-2">
         ⚠️If you reload or close this page, you will lose your progress⚠️
         <br />
