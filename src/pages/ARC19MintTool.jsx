589719ceaec07d1f4516b60b3ee0d7b68b661f2f
LoafPickle <loafpickle@protonmail.com>
2024-08-16 11:57:33 -0400

    Update ARC19MintTool.jsx

--- src/pages/ARC19MintTool.jsx
+++ src/pages/ARC19MintTool.jsx
@@ -359,7 +359,7 @@ export function ARC19MintTool() {
           )}
         </div>
       )}
-      <p className="text-sm italic text-slate-200">Fee: 0.1A/ASA</p>
+      <p className="text-sm italic text-slate-200">Fee: Free</p>
 
       <p className="text-center text-xs text-slate-400 py-1">
         ⚠️If you reload or close this page, you will lose your progress⚠️
