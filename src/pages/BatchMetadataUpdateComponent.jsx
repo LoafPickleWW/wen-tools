674897bfba19ad95c785783fbdfa781e19bbb1e4
LoafPickle <loafpickle@protonmail.com>
2024-08-16 11:58:51 -0400

    Update BatchMetadataUpdateComponent.jsx

--- src/pages/BatchMetadataUpdateComponent.jsx
+++ src/pages/BatchMetadataUpdateComponent.jsx
@@ -211,7 +211,7 @@ export function BatchCollectionMetadataUpdate() {
           )}
         </div>
       )}
-      <p className="text-sm italic text-slate-200">Fee: 0.05A/ASA</p>
+      <p className="text-sm italic text-slate-200">Fee: Free</p>
       <p className="text-center text-xs text-slate-400 py-2">
         ⚠️If you reload or close this page, you will lose your progress⚠️
         <br />
