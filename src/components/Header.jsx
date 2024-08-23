ac34cda60034f3acd7fb63e04ed5b71936840546
bwmx <blairmcilroy@icloud.com>
2024-08-23 23:30:06 +0100

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+
 import { Link } from "react-router-dom";
 import ConnectButton from "./ConnectButton";
 import SelectNetworkComponent from "./SelectNetworkComponent";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
+import DropdownMenu from "./DropdownMenu";
 
 export function Header() {
+  const [isSidesheetOpen, setIsSidesheetOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setIsSidesheetOpen(!isSidesheetOpen);
+  };
+
   return (
     <AppBar sx={{ backgroundColor: "#1A171A" }} position="sticky">
+      <DropdownMenu isOpen={isSidesheetOpen} onClose={handleDrawerToggle} />
       <Toolbar>
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="start"
+          onClick={handleDrawerToggle}
+        >
+          <MdMenu />
+        </IconButton>
         <Link to="/">
           <img src="/TL_large_white.png" alt="logo" className="mr-2 h-12 p-1" />
         </Link>
