014f5b3ad2f19bd2036b0531538a4920e2e150f6
LoafPickle <loafpickle@protonmail.com>
2024-10-22 16:21:32 -0400

    Update home.jsx
    I think this was the best one

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -72,7 +72,7 @@ export default function Home() {
             <img
               src="./crust.png"
               alt="crust"
-              className="w-24 h-20 md:w-75 md:h-20"
+              className="w-24 h-20 md:w-40 md:h-20"
             />
           </a>
         </div>
