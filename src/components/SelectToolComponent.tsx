690e475dae81e6252002903fdcc987a8236bcac1
Sandeep Varma Mantena <133403866+SandeepVarmaManthena@users.noreply.github.com>
2024-12-31 04:53:56 +0530

    fix: size reduce & restore network toggle (#95)
    * Redesign Site per attached mockups
    
    * fix: rollback to previous version of react-icons
    
    * fix: size reduce & restore network toggle
    
    ---------
    
    Co-authored-by: SatishGAXL <satish.yadlapalli@growthaxl.com>

--- src/components/SelectToolComponent.tsx
+++ src/components/SelectToolComponent.tsx
@@ -6,11 +6,19 @@ export function SelectToolComponent() {
     <div className="text-center">
       <div className="mx-auto my-2 md:my-4">
         <div className="flex flex-col lg:flex-row items-center justify-evenly lg:gap-2 gap-4">
-          <a href="https://wallet.wen.tools/" target="blank" className="lg:w-[34%] w-[70%]">
+          <a
+            href="https://wallet.wen.tools/"
+            target="blank"
+            className="lg:w-[34%] w-[70%]"
+          >
             <img src="/wenwallet.png" alt="" />
           </a>
-          <a href="https://swap.wen.tools/" target="blank" className="lg:w-[34%] w-[70%]">
-            <img src="/wenswap.png" alt=""/>
+          <a
+            href="https://swap.wen.tools/"
+            target="blank"
+            className="lg:w-[34%] w-[70%]"
+          >
+            <img src="/wenswap.png" alt="" />
           </a>
         </div>
       </div>
@@ -19,17 +27,22 @@ export function SelectToolComponent() {
       </p>
       <div className="container gap-6 cursor-pointer grid lg:grid-cols-3 py-4">
         {TOOLS.filter((tool) => tool.category === "asset").map((tool) => (
-          <div
-            className="flex flex-col gap-3 p-12 w-[100%] h-[100%] text-[#FDFDFD] rounded-[36px] shadow bg-[#000] border-4 border-[#CCCA15] transition duration-300 hover:text-black hover:bg-gradient-to-r from-[#EAE004] to-[#FF931E] hover:border-2 relative group"
-            key={tool.id}
-          >
-            <Link to={tool.path} className="block  transform -translate-y-[-1.5rem] flex flex-col items-center justify-center group-hover:-translate-y-0 duration-300 ease-in-out ">
-              <h5 className="text-xl lg:text-4xl font-normal ">{tool.label}</h5>
-            </Link>
-            {/* Description shown on hover */}
-            <p className="font-medium text-center text-black  transition-display duration-300  group-hover:block rounded-3xl duration-300 ease-in-out">
-              {tool.description}
-            </p>
+          <div className="w-full rounded-[36px] bg-gradient-to-r from-[#E4E808] to-[#FD941D] hover:bg-none p-1">
+            <div
+              className="flex flex-col rounded-[36px] border-transparent bg-black gap-3 p-8 w-[100%] h-[100%] text-[#FDFDFD] shadow bg-[#000] transition duration-200 ease-in-out hover:text-black hover:bg-gradient-to-r from-[#E4E808] to-[#FD941D] hover:border-2 relative group"
+              key={tool.id}
+            >
+              <Link
+                to={tool.path}
+                className="block  transform -translate-y-[-1.5rem] flex flex-col items-center justify-center duration-200 ease-in-out group-hover:-translate-y-0"
+              >
+                <h5 className=" text-2xl font-medium ">{tool.label}</h5>
+              </Link>
+              {/* Description shown on hover */}
+              <p className="font-medium text-center text-black  transition-display duration-200 ease-in-out group-hover:block rounded-3xl">
+                {tool.description}
+              </p>
+            </div>
           </div>
         ))}
       </div>
@@ -38,17 +51,22 @@ export function SelectToolComponent() {
       </p>
       <div className="container gap-6 cursor-pointer grid lg:grid-cols-3 py-4">
         {TOOLS.filter((tool) => tool.category === "mint").map((tool) => (
-          <div
-            className="flex flex-col  gap-3 p-12 w-[100%] h-[100%] text-[#FDFDFD] rounded-[36px] shadow bg-[#000] border-4 border-[#CCCA15] transition duration-300 hover:text-black hover:bg-gradient-to-r from-[#EAE004] to-[#FF931E] hover:border-2 relative group"
-            key={tool.id}
-          >
-            <Link to={tool.path} className="block  transform -translate-y-[-1.5rem] flex flex-col items-center justify-center duration-300 ease-in-out group-hover:-translate-y-0">
-              <h5 className=" text-3xl font-medium ">{tool.label}</h5>
-            </Link>
-            {/* Description shown on hover */}
-            <p className="font-medium text-center text-black  transition-display duration-300  group-hover:block rounded-3xl">
-              {tool.description}
-            </p>
+          <div className="w-full rounded-[36px] bg-gradient-to-r from-[#E4E808] to-[#FD941D] hover:bg-none p-1">
+            <div
+              className="flex flex-col rounded-[36px] border-transparent bg-black gap-3 p-8 w-[100%] h-[100%] text-[#FDFDFD] shadow bg-[#000] transition duration-200 ease-in-out hover:text-black hover:bg-gradient-to-r from-[#E4E808] to-[#FD941D] hover:border-2 relative group"
+              key={tool.id}
+            >
+              <Link
+                to={tool.path}
+                className="block  transform -translate-y-[-1.5rem] flex flex-col items-center justify-center duration-200 ease-in-out group-hover:-translate-y-0"
+              >
+                <h5 className=" text-2xl font-medium ">{tool.label}</h5>
+              </Link>
+              {/* Description shown on hover */}
+              <p className="font-medium text-center text-black  transition-display duration-200 ease-in-out group-hover:block rounded-3xl">
+                {tool.description}
+              </p>
+            </div>
           </div>
         ))}
       </div>
