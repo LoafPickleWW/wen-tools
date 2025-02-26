3556c70b5b216e10fb508f6487d4c4518fac08d7
LoafPickle <loafpickle@protonmail.com>
2025-02-26 13:02:10 -0500

    Update CarouselComponent.tsx

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -11,7 +11,7 @@ const CarouselComponent = ({ images }: { images: Image[] }) => {
       setCurrentIndex((prevIndex) =>
         prevIndex === images.length - 1 ? 0 : prevIndex + 1
       );
-    }, 6000);
+    }, 8000);
     return () => clearInterval(interval);
   }, [currentIndex, images.length]);
 
