b72548b954119a40b293db8291606000c4ffcf2d
LoafPickle <loafpickle@protonmail.com>
2025-08-14 20:17:34 -0400

    Update ReallySimpleMint.tsx
    Updated copy

--- src/pages/ReallySimpleMint.tsx
+++ src/pages/ReallySimpleMint.tsx
@@ -191,7 +191,7 @@ export function ReallySimpleMint() {
           </label>
           <input
             type="text"
-            placeholder="Ex: USAlgo 001"
+            placeholder="Ex: My First NFT"
             className="w-64 bg-gray-300 text-sm font-medium text-center leading-none text-black placeholder:text-black/30 px-3 py-2 border rounded border-gray-200"
             maxLength={32}
             required
@@ -310,3 +310,4 @@ export function ReallySimpleMint() {
     </div>
   );
 }
+
