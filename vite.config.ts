63eca659900f1ece2d8724f7fa7bab42338b7f23
LoafPickle <loafpickle@protonmail.com>
2026-04-30 12:31:55 -0400

    feat: implement P2P chat page with WebRTC and Liquid signal support

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,19 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills(), basicSsl()],
+  server: {
+    proxy: {
+      '/socket.io': {
+        target: 'https://debug.liquidauth.com',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+      '/attestation': {
+        target: 'https://debug.liquidauth.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 });
